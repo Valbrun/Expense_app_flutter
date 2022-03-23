58cb1bec58bc147fd05bda0376408be0d0f4e6e4
Valbrun <woodsen.v1@gmail.com>
2022-03-23 01:06:34 -0400

    first

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:ui';
+
+import 'tansaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expensae',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // const MyHomePage({Key? key}) : super(key: key);
+  final List<Transaction> transactions = [
+    Transaction(id: '1', title: 't1', amount: 69.99, date: DateTime.now()),
+    Transaction(id: '2', title: 't2', amount: 79.99, date: DateTime.now()),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('dsf;kdsjfno'),
+              elevation: 5,
+            ),
+          ),
+          // column
+          Column(
+            children: transactions
+                .map((tx) => Card(
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 2,
+                                style: BorderStyle.solid,
+                                color: Colors.black,
+                              ),
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              // tx.amount.toString(),
+                              '\$${tx.amount}',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                tx.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                // tx.date.toString(),
+                                DateFormat.MMMMd().format(tx.date),
+                                style: TextStyle(
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                .toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
